680c49afa44867ad2d77129941b5eecb631b57ce
Ijlal Dhisa <ijlalwindhi15@gmail.com>
2025-01-13 14:44:13 +0700

    feat(metadata): add metadata constants for project details and Open Graph configuration

---
+++ constants/metadata.ts
@@ -0,0 +1,17 @@
+export const METADATA = {
+  creator: "Dhisa Production",
+  description:
+    "Skeleton project Nextjs 15 with React Query, Tailwind CSS, Shadcn and TypeScript",
+  keyword: "nextjs, react-query, tailwindcss, shadcn, typescript",
+  authors: {
+    name: "Dhisa Production",
+    url: process.env.DOMAIN,
+  },
+  openGraph: {
+    url: process.env.DOMAIN,
+    siteName: "Dhisa Production",
+    locale: "en_US",
+  },
+  exTitle: "| Dhisa Production",
+  profile: "",
+};
