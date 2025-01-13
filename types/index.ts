c420fedaffd1114c95a043c9c2ab0c2c4049f418
Ijlal Dhisa <ijlalwindhi15@gmail.com>
2025-01-13 11:08:59 +0700

    feat(types): reorganize type exports and create module structure

--- types/index.ts
+++ types/index.ts
@@ -1,27 +1,2 @@
-// Theme type
-export interface IThemeStore {
-  isLoading: boolean;
-  isSidebarOpen: boolean;
-  setLoading: (loading: boolean) => void;
-  toggleSidebar: () => void;
-}
-
-// Response type
-type Data<T = unknown> = T | null;
-export interface ApiResponse<T> {
-  data: T;
-  message: string;
-  status: number;
-}
-
-export interface ResponsePagination<T = unknown> {
-  results: Data<T>;
-  page: number;
-  page_size: number;
-  count: number;
-  page_count: number;
-}
-
-export interface ResponseMessage {
-  message: string;
-}
+export * from "./modules";
+export * from "./common";
