6da56161edf51f4e8d7a134d6e6acf20af70ef45
Ijlal Dhisa <ijlalwindhi15@gmail.com>
2025-01-14 14:00:01 +0700

    feat(theme): add sidebar state management and toggle functionality

--- types/modules/theme.ts
+++ types/modules/theme.ts
@@ -1,8 +1,10 @@
 export interface IThemeStore {
   isLoading: boolean;
+  isSidebarOpen: boolean;
   modalSuccess: IModalSuccess;
   setLoading: (loading: boolean) => void;
   setModalSuccess: (modal: IModalSuccess) => void;
+  toggleSidebar: () => void;
 }
 
 interface IModalSuccess {
@@ -19,4 +21,17 @@ interface IModalSuccess {
     | "link";
   animation?: "success" | "email";
   action?: () => void;
+}
+
+// Header Dashboard
+export interface INavItem {
+  title: string;
+  path: string;
+  icon: string;
+  sub: boolean | ISubNavItem[];
+}
+
+export interface ISubNavItem {
+  title: string;
+  path: string;
 }
