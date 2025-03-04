da8b1ed0c22916e5b21e7b3bbfb261ade86ba7f1
Ijlal Windhi <73152962+ijlalWindhi@users.noreply.github.com>
2025-03-04 11:50:06 +0700

    feat(talent-mapping): enhance DetailTalent component with tabs for personal and mapping information

--- features/talent-mapping/components/DetailTalent.tsx
+++ features/talent-mapping/components/DetailTalent.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import dynamic from "next/dynamic";
+import { Pencil } from "lucide-react";
 
 import {
   Sheet,
@@ -7,27 +9,65 @@ import {
   SheetHeader,
   SheetTitle,
 } from "@/components/ui/sheet";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Button } from "@/components/ui/button";
+import HeaderDetailTalent from "./HeaderDetail";
+const PersonalInformationDetail = dynamic(
+  () => import("./PersonalInformationDetail"),
+);
+const MappingInformationDetail = dynamic(
+  () => import("./MappingInformationDetail"),
+);
 
 import useTalentMapping from "@/stores/talent-mapping";
 
 export default function DetailTalent() {
   // variables
-  const { modalDetailTalentMapping, toggleModalDetailTalentMapping } =
-    useTalentMapping();
+  const {
+    modalDetailTalentMapping,
+    toggleModalDetailTalentMapping,
+    toggleModalTalentMapping,
+  } = useTalentMapping();
+
   return (
     <Sheet
       open={modalDetailTalentMapping}
-      onOpenChange={() => {
-        console.log("MASUK PAK LEK");
-        toggleModalDetailTalentMapping(false);
-      }}
+      onOpenChange={() => toggleModalDetailTalentMapping(false)}
     >
       <SheetContent className="!min-w-[100vw] md:!min-w-[60vw] lg:!min-w-[40vw] overflow-y-auto">
         <SheetHeader>
-          <SheetTitle>Detail Ticket</SheetTitle>
+          <SheetTitle>View Talent</SheetTitle>
+          <HeaderDetailTalent />
         </SheetHeader>
+        <Tabs defaultValue="personal_information" className="min-w-full mt-2">
+          <TabsList className="w-full">
+            <TabsTrigger value="personal_information">
+              Personal Information
+            </TabsTrigger>
+            <TabsTrigger value="mapping_information">
+              Mapping Information
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent value="personal_information">
+            <PersonalInformationDetail />
+          </TabsContent>
+          <TabsContent value="mapping_information">
+            <MappingInformationDetail />
+          </TabsContent>
+        </Tabs>
+        <SheetFooter>
+          <Button
+            className="w-full"
+            type="submit"
+            onClick={() => {
+              toggleModalTalentMapping(true);
+              toggleModalDetailTalentMapping(false);
+            }}
+          >
+            <Pencil size={16} className="mr-2" /> Edit Data
+          </Button>
+        </SheetFooter>
       </SheetContent>
-      <SheetFooter></SheetFooter>
     </Sheet>
   );
 }
