12d5c05600ce2945476bd6f3e5bcfcaceab08ad5
Ijlal Windhi <73152962+ijlalWindhi@users.noreply.github.com>
2025-03-07 22:53:06 +0700

    feat(user-management): enhance UserManagement component with search and pagination features

--- features/user-management/components/UserManagement.tsx
+++ features/user-management/components/UserManagement.tsx
@@ -1,5 +1,79 @@
+"use client";
 import React from "react";
+import { Plus } from "lucide-react";
+import dynamic from "next/dynamic";
+
+import {
+  Card,
+  CardHeader,
+  CardContent,
+  CardFooter,
+  CardTitle,
+} from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import InputSearch from "@/components/common/input-search";
+import { PaginationCompo } from "@/components/ui/pagination";
+import List from "./List";
+const ModalUserManagement = dynamic(() => import("./ModalUserManagement"));
+
+import useUserManagement from "@/stores/user-management";
 
 export default function UserManagement() {
-  return <div>UserManagement</div>;
+  // variables
+  const { toggleModalUserManagement } = useUserManagement();
+
+  // functions
+  const handleSearch = (searchTerm: string) => {
+    try {
+      console.log(searchTerm);
+    } catch (error) {
+      console.error("Error from handleSearch: ", error);
+    }
+  };
+
+  return (
+    <Card>
+      <CardHeader className="flex flex-col gap-2 md:flex-row md:justify-between md:items-center md:gap-4 w-full">
+        <div className="flex flex-col sm:flex-row gap-2 w-full md:w-[40%]">
+          <CardTitle className="font-semibold">User List</CardTitle>
+          <Badge variant={"outline"} className="w-fit">
+            <span className="text-primary">•</span> Total 100 User
+          </Badge>
+        </div>
+        <div className="flex flex-col sm:flex-row w-full md:w-[60%] items-center justify-end gap-2">
+          <div className="w-full md:w-1/3 xl:w-1/4">
+            <InputSearch
+              onSearch={handleSearch}
+              placeholder="Search user here..."
+              defaultValue={""}
+            />
+          </div>
+          <Button
+            size="sm"
+            className="w-full md:w-auto"
+            onClick={() => toggleModalUserManagement(true)}
+          >
+            <Plus size={16} />
+            Register User
+          </Button>
+        </div>
+      </CardHeader>
+      <CardContent>
+        <List />
+      </CardContent>
+      <CardFooter>
+        <PaginationCompo
+          meta={{
+            page: 1,
+            page_size: 10,
+            count: 100,
+            page_count: 10,
+          }}
+          onPageChange={(page) => console.log(page)}
+        />
+      </CardFooter>
+      <ModalUserManagement />
+    </Card>
+  );
 }
