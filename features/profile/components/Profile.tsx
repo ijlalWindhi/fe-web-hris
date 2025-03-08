097d44e64e60bb19408adf518401c75f61c3db5b
Ijlal Windhi <73152962+ijlalWindhi@users.noreply.github.com>
2025-03-08 23:39:39 +0700

    feat(profile): implement Profile component with user details and logout functionality

---
+++ features/profile/components/Profile.tsx
@@ -0,0 +1,53 @@
+"use client";
+import React from "react";
+import { LogOut } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import DataRow from "@/components/common/data-row";
+import InputProfile from "@/components/common/input-profile";
+
+import { truncateText } from "@/utils/truncate";
+import { useIsMobile } from "@/hooks/use-mobile";
+
+export default function Profile() {
+  // variables
+  const isMobile = useIsMobile();
+
+  return (
+    <div className="w-full">
+      <div className="w-full h-20 md:h-36 bg-gradient-to-tr from-blue-200 to-blue-400 rounded-2xl relative">
+        <div className="absolute -bottom-10 md:-bottom-12 w-full">
+          <div className="w-full flex justify-between items-end px-4 md:px-10">
+            <div className="flex items-end gap-2 md:gap-4">
+              <InputProfile
+                width="w-16 md:w-24"
+                height="h-16 md:h-24"
+                onFileChange={(file) => console.log(file)}
+              />
+              <div>
+                <h1 className="font-semibold md:text-lg lg:text-2xl">
+                  {truncateText("Paimen Bin Kasimen", isMobile ? 16 : 100)}
+                </h1>
+                <p className="text-xs md:text-sm">Super Admin</p>
+              </div>
+            </div>
+            <Button className="mt-2" variant="outline">
+              <LogOut />
+              Logout
+            </Button>
+          </div>
+        </div>
+      </div>
+      <Card className="mt-16 md:mt-20">
+        <dl className="divide-y">
+          <DataRow label="User ID" value={"-"} />
+          <DataRow label="Name" value={"-"} />
+          <DataRow label="Email Address" value={"-"} />
+          <DataRow label="Number Phone" value={"-"} />
+          <DataRow label="Address" value={"-"} />
+        </dl>
+      </Card>
+    </div>
+  );
+}
