c3d4b251d9520f2f69dc16f7dd429c88437f9f38
Ijlal Windhi <73152962+ijlalWindhi@users.noreply.github.com>
2025-03-09 21:12:41 +0700

    feat(master-client): integrate toggle function for adding outlets in OutletEmpty component

--- features/master-client/components/OutletEmpty.tsx
+++ features/master-client/components/OutletEmpty.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Lottie from "@/components/common/lottie";
-
-import EmptyAnimation from "@/public/animations/empty.json";
-import { Button } from "@/components/ui/button";
 import { Plus } from "lucide-react";
 
+import { Button } from "@/components/ui/button";
+
+import useMasterClient from "@/stores/master-client";
+import EmptyAnimation from "@/public/animations/empty.json";
+
 export default function OutletEmpty() {
+  // variables
+  const { toggleModalAddOutlet } = useMasterClient();
   return (
     <div className="flex flex-col items-center justify-center gap-2 my-2">
       <Lottie animation={EmptyAnimation} height={150} width={150} />
@@ -14,7 +18,12 @@ export default function OutletEmpty() {
         It looks like there are no outlets available. Start adding one now to
         get started.
       </p>
-      <Button className="w-fit" variant={"default-outline"} size="sm">
+      <Button
+        className="w-fit"
+        variant={"default-outline"}
+        size="sm"
+        onClick={() => toggleModalAddOutlet(true)}
+      >
         <Plus size={16} />
         Add Outlet
       </Button>
