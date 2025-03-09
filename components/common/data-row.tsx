936acaf9330e0a0a7525895c703a0c87b1dd0998
Ijlal Windhi <73152962+ijlalWindhi@users.noreply.github.com>
2025-03-09 20:00:31 +0700

    feat(data-row): add bold prop for customizable label and value styling

--- components/common/data-row.tsx
+++ components/common/data-row.tsx
@@ -1,19 +1,29 @@
+import { cn } from "@/utils/utils";
 import Image from "next/image";
 
 interface IDataRowProps {
   label: string;
   value: string | null;
   type?: "text" | "image";
+  bold?: boolean;
 }
 
 export default function DataRow({
   label,
   value,
   type = "text",
+  bold = false,
 }: Readonly<IDataRowProps>) {
   return (
     <div className="flex flex-col md:flex-row justify-start py-3 border-b first:border-b-0 last:border-b-0">
-      <dt className="text-sm font-medium text-muted-foreground w-48">
+      <dt
+        className={cn(
+          "text-sm w-48",
+          bold
+            ? "font-semibold text-black"
+            : "font-medium text-muted-foreground",
+        )}
+      >
         {label}
       </dt>
       <span className="pr-2 hidden md:block w-2">:</span>
@@ -29,7 +39,12 @@ export default function DataRow({
           </div>
         </dd>
       ) : (
-        <dd className="text-sm text-left md:text-right">
+        <dd
+          className={cn(
+            "text-sm text-left md:text-right",
+            bold ? "font-semibold text-black" : "",
+          )}
+        >
           {(!Array.isArray(value) && value) || "-"}
         </dd>
       )}
