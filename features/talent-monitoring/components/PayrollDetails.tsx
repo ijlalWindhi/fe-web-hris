32ea36363fd1798e680e52879d3aa15b977001a4
Ijlal Windhi <73152962+ijlalWindhi@users.noreply.github.com>
2025-03-10 18:52:08 +0700

    feat(talent-monitoring): add PayrollDetails component with contract history table and types

--- features/talent-monitoring/components/PayrollDetails.tsx
+++ features/talent-monitoring/components/PayrollDetails.tsx
@@ -1,5 +1,85 @@
 import React from "react";
 
+import { ITableHeader } from "@/components/ui/table";
+import { Table } from "@/components/common/table";
+
+import { IPayrollDetailsTaletMonitoring } from "@/types";
+
+const TableHeader: ITableHeader[] = [
+  {
+    key: "month",
+    title: "Month",
+    className: "min-w-[8rem]",
+  },
+  {
+    key: "gaji_pokok",
+    title: "Gaji Pokok",
+    className: "min-w-[11rem]",
+  },
+  {
+    key: "tunjangan_makan",
+    title: "Tunjangan Makan",
+    className: "min-w-[11rem]",
+  },
+  {
+    key: "tunjangan_bensin",
+    title: "Tunjangan Bensin",
+    className: "min-w-[11rem]",
+  },
+  {
+    key: "bonus_performance",
+    title: "Bonus Performance",
+    className: "min-w-[11rem]",
+  },
+  {
+    key: "potongan_pajak",
+    title: "Potongan Pajak",
+    className: "min-w-[11rem]",
+  },
+  {
+    key: "agency_fee",
+    title: "Agency Fee (%)",
+    className: "min-w-[11rem]",
+  },
+  {
+    key: "total",
+    title: "Total",
+    className: "min-w-[11rem]",
+  },
+];
+
 export default function PayrollDetails() {
-  return <div>PayrollDetails</div>;
+  return (
+    <div className="border rounded-xl p-4 space-y-2">
+      <h2 className="md:text-lg font-semibold">Payroll Details</h2>
+      <Table<IPayrollDetailsTaletMonitoring>
+        header={TableHeader}
+        data={[
+          {
+            id: "1",
+            month: "January 2021",
+            gaji_pokok: "Rp 1.000.000",
+            tunjangan_makan: "Rp 500.000",
+            tunjangan_bensin: "Rp 0",
+            bonus_performance: "Rp 0",
+            potongan_pajak: "Rp 0",
+            agency_fee: "5%",
+            total: "Rp 1.500.000",
+          },
+          {
+            id: "2",
+            month: "February 2021",
+            gaji_pokok: "Rp 1.000.000",
+            tunjangan_makan: "Rp 500.000",
+            tunjangan_bensin: "Rp 0",
+            bonus_performance: "Rp 0",
+            potongan_pajak: "Rp 0",
+            agency_fee: "5%",
+            total: "Rp 1.500.000",
+          },
+        ]}
+        loading={false}
+      ></Table>
+    </div>
+  );
 }
--- types/modules/talent-monitoring.ts
+++ types/modules/talent-monitoring.ts
@@ -14,3 +14,15 @@ export interface IHistoryContractTalentMonitoring {
   end_date: string;
   contract_document: string;
 }
+
+export interface IPayrollDetailsTaletMonitoring {
+  id: string;
+  month: string;
+  gaji_pokok: string;
+  tunjangan_makan: string;
+  tunjangan_bensin: string;
+  bonus_performance: string;
+  potongan_pajak: string;
+  agency_fee: string;
+  total: string;
+}
