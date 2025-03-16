c48c5ae1a7ad6bfdecaeaaaaff5af4d30df30f72
Ijlal Windhi <73152962+ijlalWindhi@users.noreply.github.com>
2025-03-17 00:07:46 +0700

    feat(profile): fetch and display user profile data in the Profile component

--- features/profile/components/Profile.tsx
+++ features/profile/components/Profile.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import { LogOut } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
@@ -9,10 +9,17 @@ import InputProfile from "@/components/common/input-profile";
 
 import { truncateText } from "@/utils/truncate";
 import { useIsMobile } from "@/hooks/use-mobile";
+import useAuth from "@/stores/auth";
 
 export default function Profile() {
   // variables
   const isMobile = useIsMobile();
+  const { profile, getProfile } = useAuth();
+
+  // lifecycle
+  useEffect(() => {
+    getProfile();
+  }, [getProfile]);
 
   return (
     <div className="w-full">
@@ -23,13 +30,15 @@ export default function Profile() {
               <InputProfile
                 width="w-16 md:w-24"
                 height="h-16 md:h-24"
-                onFileChange={(file) => console.log(file)}
+                shouldChange={false}
               />
               <div>
                 <h1 className="font-semibold md:text-lg lg:text-2xl">
-                  {truncateText("Paimen Bin Kasimen", isMobile ? 16 : 100)}
+                  {truncateText(profile?.name, isMobile ? 16 : 100)}
                 </h1>
-                <p className="text-xs md:text-sm">Super Admin</p>
+                <p className="text-xs md:text-sm">
+                  {profile?.role?.name ?? "-"}
+                </p>
               </div>
             </div>
             <Button className="mt-2" variant="outline">
@@ -41,11 +50,11 @@ export default function Profile() {
       </div>
       <Card className="mt-16 md:mt-20">
         <dl className="divide-y">
-          <DataRow label="User ID" value={"-"} />
-          <DataRow label="Name" value={"-"} />
-          <DataRow label="Email Address" value={"-"} />
-          <DataRow label="Number Phone" value={"-"} />
-          <DataRow label="Address" value={"-"} />
+          <DataRow label="User ID" value={profile?.id} />
+          <DataRow label="Name" value={profile?.name} />
+          <DataRow label="Email Address" value={profile?.email} />
+          <DataRow label="Number Phone" value={profile?.phone} />
+          <DataRow label="Address" value={profile?.email} />
         </dl>
       </Card>
     </div>
