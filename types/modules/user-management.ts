a10b8d4daf7ebb8a25e313e6af7726686f88f686
Ijlal Windhi <73152962+ijlalWindhi@users.noreply.github.com>
2025-03-16 21:52:22 +0700

    fix(user-management): rename selectedId to selectedData and update user management types for clarity

--- types/modules/user-management.ts
+++ types/modules/user-management.ts
@@ -1,17 +1,33 @@
 export interface IUserManagementStore {
   modalUserManagement: boolean;
-  selectedId: null | number;
+  selectedData: IResponseUserManagement | null;
   toggleModalUserManagement: (isOpen: boolean) => void;
-  setSelectedId: (id: number | null) => void;
+  setSelectedData: (data: IResponseUserManagement | null) => void;
 }
 
-export interface IResponseListUserManagement {
-  id: number;
+export interface IResponseUserManagement {
+  id_user: string;
   name: string;
   email: string;
   phone: string;
   address: string;
-  client_name: string;
-  role: string;
-  status: string;
+  role: {
+    id: number;
+    name: string;
+  };
+  client: {
+    id: number | null;
+    name: string | null;
+  };
+  status: boolean;
+  photo: string;
 }
+
+export type TPayloadUserManagement = {
+  name: string;
+  email: string;
+  phone: string;
+  role_id: number;
+  address: string;
+  photo: string;
+};
