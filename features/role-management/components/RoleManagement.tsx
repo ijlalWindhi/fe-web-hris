798595f655d808e55cfc477588c2c48d39157231
Ijlal Windhi <73152962+ijlalWindhi@users.noreply.github.com>
2025-03-16 23:34:31 +0700

    feat(role-management): enhance role management component with dynamic role rendering and search functionality

--- features/role-management/components/RoleManagement.tsx
+++ features/role-management/components/RoleManagement.tsx
@@ -1,5 +1,7 @@
 "use client";
-import React from "react";
+import React, { useCallback, useState } from "react";
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
 
 import {
   Card,
@@ -12,15 +14,59 @@ import { Badge } from "@/components/ui/badge";
 import InputSearch from "@/components/common/input-search";
 import { PaginationCompo } from "@/components/ui/pagination";
 import { Button } from "@/components/ui/button";
-import Link from "next/link";
+
+import { useRoleList } from "../hooks/useRoleManagement";
+import { TSearchParams } from "@/types";
+import { useSetParams } from "@/utils/set-params";
 
 export default function RoleManagement() {
+  // variables
+  const updateParams = useSetParams();
+  const searchParams = useSearchParams();
+  const [queryParams, setQueryParams] = useState<TSearchParams>({
+    page: parseInt(searchParams.get("page") ?? "1"),
+    page_size: parseInt(searchParams.get("page_size") ?? "10"),
+    src: searchParams.get("src") ?? undefined,
+  });
+  const { data } = useRoleList(queryParams);
+
   // functions
-  const handleSearch = (searchTerm: string) => {
+  const handleSearch = useCallback(
+    async (searchTerm: string) => {
+      try {
+        const newParams: TSearchParams = {
+          ...queryParams,
+          src: searchTerm || undefined,
+          page: 1,
+        };
+
+        setQueryParams(newParams);
+        updateParams(newParams);
+      } catch (error) {
+        console.error("Error from handleSearch: ", error);
+      }
+    },
+    [queryParams, updateParams],
+  );
+
+  const handlePageChange = async ({
+    page,
+    pageSize,
+  }: {
+    page: number;
+    pageSize: number;
+  }) => {
     try {
-      console.log(searchTerm);
+      const newParams: TSearchParams = {
+        ...queryParams,
+        page: page,
+        page_size: pageSize,
+      };
+
+      setQueryParams(newParams);
+      updateParams(newParams);
     } catch (error) {
-      console.error("Error from handleSearch: ", error);
+      console.error("Error from handlePageChange:", error);
     }
   };
 
@@ -30,84 +76,74 @@ export default function RoleManagement() {
         <div className="flex flex-col sm:flex-row gap-2 w-full md:w-[40%]">
           <CardTitle className="font-semibold">Role List</CardTitle>
           <Badge variant={"outline"} className="w-fit">
-            <span className="text-primary">•</span> Total 100 Role
+            <span className="text-primary">•</span> Total{" "}
+            {data?.meta?.count ?? 0} Role
           </Badge>
         </div>
         <div className="flex flex-col sm:flex-row w-full md:w-[60%] items-center justify-end gap-2">
           <InputSearch
             onSearch={handleSearch}
             placeholder="Search role here..."
-            defaultValue={""}
+            defaultValue={queryParams.src}
           />
         </div>
       </CardHeader>
       <CardContent>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 md:gap-4">
-          <div className="space-y-2 p-4 border rounded-lg">
-            <div className="flex items-center gap-3">
-              <h2 className="font-medium">Super Admin</h2>
-              <Badge variant={"outline"} className="w-fit">
-                <span className="text-primary">•</span> Total 100 User
-              </Badge>
-            </div>
-            <ul className="space-y-1">
-              <li className="text-sm">
-                <span className="text-primary mr-1">•</span> View User
-              </li>
-              <li className="text-sm">
-                <span className="text-primary mr-1">•</span> Create User
-              </li>
-              <li className="text-sm">
-                <span className="text-primary mr-1">•</span> Update User
-              </li>
-              <li className="text-sm">
-                <span className="text-primary mr-1">•</span> And 10 more...
-              </li>
-            </ul>
-            <Link href="/user-management/role-management/super-admin">
-              <Button variant={"outline"} size={"sm"} className="!mt-4 w-1/2">
-                View More
-              </Button>
-            </Link>
-          </div>
-          <div className="space-y-2 p-4 border rounded-lg">
-            <div className="flex items-center gap-3">
-              <h2 className="font-medium">Admin</h2>
-              <Badge variant={"outline"} className="w-fit">
-                <span className="text-primary">•</span> Total 100 User
-              </Badge>
-            </div>
-            <ul className="space-y-1">
-              <li className="text-sm">
-                <span className="text-primary mr-1">•</span> View User
-              </li>
-              <li className="text-sm">
-                <span className="text-primary mr-1">•</span> Create User
-              </li>
-              <li className="text-sm">
-                <span className="text-primary mr-1">•</span> Update User
-              </li>
-              <li className="text-sm">
-                <span className="text-primary mr-1">•</span> And 10 more...
-              </li>
-            </ul>
-            <Link href="/user-management/role-management/super-admin">
-              <Button variant={"outline"} size={"sm"} className="!mt-4 w-1/2">
-                View More
-              </Button>
-            </Link>
-          </div>
+          {data?.data?.map((role) => {
+            const permissions = role?.permission || [];
+            const displayedPermissions = permissions.slice(0, 4);
+            const remainingPermissions = permissions.length - 4;
+
+            return (
+              <div key={role.id} className="space-y-2 p-4 border rounded-lg">
+                <div className="flex items-center gap-3">
+                  <h2 className="font-medium capitalize">{role.name}</h2>
+                  <Badge variant={"outline"} className="w-fit">
+                    <span className="text-primary">•</span> Total{" "}
+                    {role.total_user} User
+                  </Badge>
+                </div>
+                <ul className="space-y-1">
+                  {displayedPermissions.map((permission) => (
+                    <li key={permission.id} className="text-sm capitalize">
+                      <span className="text-primary mr-1">•</span>{" "}
+                      {permission.permission ?? "-"}{" "}
+                      {permission.module?.nama ?? "-"}
+                    </li>
+                  ))}
+                  {remainingPermissions > 0 && (
+                    <li className="text-sm capitalize">
+                      <span className="text-primary mr-1">•</span> And{" "}
+                      {remainingPermissions} more...
+                    </li>
+                  )}
+                </ul>
+                <Link href={`/user-management/role-management/${role.id}`}>
+                  <Button
+                    variant={"outline"}
+                    size={"sm"}
+                    className="!mt-4 w-1/2"
+                  >
+                    View More
+                  </Button>
+                </Link>
+              </div>
+            );
+          })}
         </div>
       </CardContent>
       <CardFooter>
         <PaginationCompo
           meta={{
-            page: 1,
-            page_size: 10,
-            count: 100,
-            page_count: 10,
+            page: queryParams.page,
+            page_size: queryParams.page_size,
+            count: data?.meta?.count ?? 0,
+            page_count: Math.ceil(
+              (data?.meta?.count ?? 0) / queryParams.page_size,
+            ),
           }}
-          onPageChange={(page) => console.log(page)}
+          onPageChange={(data) => handlePageChange(data)}
         />
       </CardFooter>
     </Card>
