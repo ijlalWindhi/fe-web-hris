7195ae8759cb2631f2f8649ddc474fa126e9117c
Ijlal Windhi <73152962+ijlalWindhi@users.noreply.github.com>
2025-03-17 20:01:19 +0700

    feat(master-client): update IMasterClientStore and IResponseListMasterClient interfaces for improved data handling and structure

--- types/modules/master-client.ts
+++ types/modules/master-client.ts
@@ -3,22 +3,59 @@ export interface IMasterClientStore {
   modalDetailMasterClient: boolean;
   modalDownloadReport: boolean;
   modalAddOutlet: boolean;
-  selectedId: null | number;
+  selectedData: IResponseListMasterClient | null;
   selectedIdOutlet: null | string;
   toggleModalMasterClient: (isOpen: boolean) => void;
   toggleModalDetailMasterClient: (isOpen: boolean) => void;
   toggleModalDownloadReport: (isOpen: boolean) => void;
   toggleModalAddOutlet: (isOpen: boolean) => void;
-  setSelectedId: (id: number | null) => void;
+  setSelectedData: (id: IResponseListMasterClient | null) => void;
   setSelectedOutletId: (id: string | null) => void;
 }
 
 export interface IResponseListMasterClient {
-  id: number;
+  id: string;
   name: string;
   address: string;
-  outlet: string[];
+  payment_date: string;
+  outlet: {
+    id: number;
+    name: string;
+    address: string;
+    created_at: string;
+    isact: boolean;
+  }[];
+  cs_person: string;
+  cs_number: string;
+  cs_email: string;
+  created_at: string;
+  isact: boolean;
 }
+
+export type TPayloadMasterClient = {
+  name: string;
+  address: string;
+  outlet: {
+    name: string;
+    latitude: number;
+    longitude: number;
+    address: string;
+  }[];
+  basic_salary: number;
+  agency_fee: number;
+  payment_date: string;
+  bpjs: {
+    name: string;
+    amount: number;
+  }[];
+  allowences: {
+    name: string;
+    amount: number;
+  }[];
+  cs_person: string;
+  cs_number: string;
+  cs_email: string;
+};
 
 export interface IListOutlet {
   id: number;
