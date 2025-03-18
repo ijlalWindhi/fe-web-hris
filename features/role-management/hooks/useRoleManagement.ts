c457e42187da51585057493632cb7ad833bb8318
Ijlal Windhi <73152962+ijlalWindhi@users.noreply.github.com>
2025-03-18 23:16:54 +0700

    feat(role-management): add hooks for role detail and user role retrieval

--- features/role-management/hooks/useRoleManagement.ts
+++ features/role-management/hooks/useRoleManagement.ts
@@ -1,4 +1,8 @@
-import { getListRoleManagement } from "@/services/role-management";
+import {
+  getListRoleManagement,
+  getDetailRoleManagement,
+  getUserRoleManagement,
+} from "@/services/role-management";
 import { TSearchParams } from "@/types";
 import { useQuery } from "@tanstack/react-query";
 
@@ -6,5 +10,19 @@ export function useRoleList(params: TSearchParams) {
   return useQuery({
     queryKey: ["roleList", params],
     queryFn: () => getListRoleManagement(params),
+  });
+}
+
+export function useRoleDetail(id: string) {
+  return useQuery({
+    queryKey: ["roleDetail", id],
+    queryFn: () => getDetailRoleManagement(id),
+  });
+}
+
+export function useUserRole(id: string, params: TSearchParams) {
+  return useQuery({
+    queryKey: ["userRole", id, params],
+    queryFn: () => getUserRoleManagement(id, params),
   });
 }
