4e1b46f54c50edb4917b749d6a1a4e073ff6719d
Ijlal Windhi <73152962+ijlalWindhi@users.noreply.github.com>
2025-03-18 23:15:11 +0700

    feat(role-management): refactor permission structure in role management interfaces for improved type safety

--- types/modules/role-management.ts
+++ types/modules/role-management.ts
@@ -1,13 +1,22 @@
+export interface IPermission {
+  id: number;
+  permission: string;
+  module: {
+    id: number;
+    nama: string;
+  };
+}
+
 export interface IResponseRoleManagement {
   id: number;
   name: string;
   total_user: number;
-  permission: {
-    id: number;
-    permission: string;
-    module: {
-      id: number;
-      nama: string;
-    };
-  }[];
+  permission: IPermission[];
+}
+
+export interface IResponseDetailRoleManagement {
+  id_role: number;
+  role_name: string;
+  total_user: number;
+  permission: IPermission[];
 }
