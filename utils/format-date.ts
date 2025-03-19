61b84b905b5af520b177ab88eee9f4904432e56e
Ijlal Windhi <73152962+ijlalWindhi@users.noreply.github.com>
2025-03-19 09:44:32 +0700

    feat(format-date): enhance date formatting function to accept custom input and output formats

--- utils/format-date.ts
+++ utils/format-date.ts
@@ -2,14 +2,30 @@ import { format, parse } from "date-fns";
 
 /**
  * Function for changing date format from "DD-MMMM-YYYY" to "DD-MM-YYYY"
- * @param inputDate
+ * @param {string} inputDate - Date string to be formatted
+ * @param {string} formatFrom - Format of the input date
+ * @param {string} formatTo - Format of the output date
  * @returns
  * @example
- * formatDate("01 August 2021") => "01-08-2021"
+ * formatDate({
+ *  inputDate: "01 January 2021",
+ *  formatFrom: "dd MMMM yyyy",
+ *  formatTo: "dd-MM-yyyy",
+ * });
  */
-export function formatDate(inputDate: string): string {
-  const parsedDate = parse(inputDate, "dd MMMM yyyy", new Date());
-  const formattedDate = format(parsedDate, "dd-MM-yyyy");
+interface IFormatDateProps {
+  inputDate: string;
+  formatFrom: string;
+  formatTo: string;
+}
+
+export function formatDate({
+  inputDate,
+  formatFrom = "dd MMMM yyyy",
+  formatTo = "dd-MM-yyyy",
+}: IFormatDateProps): string {
+  const parsedDate = parse(inputDate, formatFrom, new Date());
+  const formattedDate = format(parsedDate, formatTo);
 
   return formattedDate;
 }
